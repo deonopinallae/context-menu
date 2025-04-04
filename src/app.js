1bb9dcb03181deb0af20e05f055284bd8f9e7531
Natali Khudyakova <natali272014@gmail.com>
2025-04-04 17:49:10 +0400

    Конечный вариант

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import './styles.css'
-import { ContextMenu } from './menu'
-import { BackgroundModule } from './modules/background.module'
+import "./styles.css";
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module";
 
-
-const contextMenu = new ContextMenu('#menu')
+const contextMenu = new ContextMenu("#menu");
 
 // Добавляем модули
-contextMenu.add(new BackgroundModule())
+contextMenu.add(new BackgroundModule());
