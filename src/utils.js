f53b546f3d3cb6c9d975999f1e7bac44006b2409
Natali Khudyakova <natali272014@gmail.com>
2025-04-04 17:40:30 +0400

    Общие файлы и background_module

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export function generateRandomColor() {
+  // Генерируем RGB-компоненты
+  const r = random(0, 255);
+  const g = random(0, 255);
+  const b = random(0, 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
