5e5d638ffd4fe9029f92020d62f076077cf0dcb3
Natali Khudyakova <natali272014@gmail.com>
2025-04-06 00:29:31 +0400

    Правки в main-page и в game.module

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import "./styles.css";
 import { MainPage } from "./components/main-page";
 import { ContextMenu } from "./components/menu";
-import { BackgroundModule } from "./modules/background.module";
 import { GameCrossModule } from "./modules/game.module";
 
 new MainPage('Игры', 'Выберите игру из контекстного меню');
 const contextMenu = new ContextMenu("#menu");
 
 // Добавляем модули
-contextMenu.add(new BackgroundModule());
 contextMenu.add(new GameCrossModule());
--- src/components/main-page.js
+++ src/components/main-page.js
@@ -1,15 +1,18 @@
 export class MainPage {
-    constructor(title, text){
-        this.title = document.createElement('h1')
-        this.title.textContent = title
-        this.title.className = 'main-page-info'
-        this.title.style.fontFamily = '"Great Vibes", cursive'
+  constructor(title, text) {
+    this.title = document.createElement("h1");
+    this.title.textContent = title;
+    this.title.className = "main-page-info";
+    this.title.style.fontFamily = '"Great Vibes", cursive';
 
-        this.text = document.createElement('p')
-        this.text.textContent = text
-        this.text.className = 'main-page-info'
-        this.text.style.fontFamily = '"Great Vibes", cursive'
+    this.text = document.createElement("p");
+    this.text.textContent = text;
+    this.text.className = "main-page-info";
+    this.text.style.fontFamily = '"Great Vibes", cursive';
 
-        document.body.append(this.title, this.text)
-    }
+    const mainContainer = document.createElement("div");
+    mainContainer.className = "main-container";
+
+    document.body.append(this.title, this.text, mainContainer);
+  }
 }
