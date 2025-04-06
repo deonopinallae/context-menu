e06018cedcd123dddfc5ddf68ec8c1d677069a47
다별로 <dabyeollo@dabyeolloui-MacBookAir.local>
2025-04-06 10:33:15 +0300

    Разработан шар с советами

--- src/modules/advices.module.js
+++ src/modules/advices.module.js
@@ -1,13 +1,13 @@
-import {Module} from '../core/module'
-import {random} from '../utils'
+import { Module } from '../core/module'
+import { random } from '../utils'
+// import  './modules-styles/advices.module.css'
 
-export class Advices extends Module{
-    constructor(){
+export class Advices extends Module {
+    constructor() {
         super('getAdvice', 'Получить совет')
-        this.advicesArr = fetch('https://raw.githubusercontent.com/deonopinallae/advices/main/advices.json')
-        .then(responce => responce.json())
-        .then(data => data)
-        .catch(error => console.log('error', error))
+
+        this.advices = []
+        this.loadAdvices();
 
         this.$rootElement = document.createElement('div')
         this.$rootElement.className = 'advice-ball__container'
@@ -22,27 +22,55 @@ export class Advices extends Module{
         this.$rootElement.append(this.ballImg)
 
         this.adviceText = document.createElement('p')
-        // this.adviceText.textContent = random(0, 111)
+        this.adviceText.className = ('advice__text')
+
+        // const mainContainer = document.querySelector(".main-container")
+        // mainContainer.append(this.$rootElement)
+    }
+
+    async loadAdvices() {
+        try {
+            const response = await fetch('https://raw.githubusercontent.com/deonopinallae/advices/main/advices.json')
+            const data = await response.json()
+            this.advices = data.advices || []
+        } catch (error) {
+            console.error('Ошибка загрузки:', error)
+            this.advices = ["Советы не загрузились :("]
+        }
 
     }
-    trigger(){
-        this.$rootElement.style.display = 'flex'
-        this.$rootElement.style.opacity = '0'
-        this.$rootElement.style.animationName = ''
-        this.$rootElement.style.transform = 'scale(1)'
+
+    trigger() {
+        this.ballImg.style.display = 'block'
+        this.ballImg.style.opacity = '0'
+        this.ballImg.style.animationName = ''
+        this.ballImg.style.transform = 'scale(1)'
         this.light.style.animationName = ''
         this.light.style.transform = 'scale(1)'
+        this.adviceText.style.opacity = '0'
 
         document.body.append(this.$rootElement)
-        setTimeout(() => this.$rootElement.style.opacity = '1', 100)
+        setTimeout(() => this.ballImg.style.opacity = '1', 100)
         setTimeout(() => this.light.style.animationName = 'light-animation', 500)
         setTimeout(() => {
-            this.$rootElement.style.animationName = 'container-animation'
-            this.$rootElement.addEventListener('animationend', () => this.$rootElement.style.display = 'none', { once: true })
+            this.ballImg.style.animationName = 'container-animation'
+            this.ballImg.addEventListener('animationend', () => {
+                this.ballImg.style.display = 'none'
+                this.$rootElement.append(this.adviceText)    
+                setTimeout(() => {
+                    this.adviceText.style.opacity = '1'
+                }, 300)
+                
+            }, { once: true })
         }, 4500)
 
-        console.log(this.advicesArr)
-        console.log(this.advicesArr.find(el => el[random(0, 111)]))
+        if (this.advices.length === 0) {
+            this.adviceText.textContent = "Загрузка..."
+            return
+        }
+        this.adviceText.textContent = this.advices[random(0, this.advices.length - 1)]
         
+        console.log(this.adviceText.textContent)
+
     }
 }
